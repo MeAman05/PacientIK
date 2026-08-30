7f95c9eb390bace1b8afb9189d35e766e9120303
Aman <kekmek366@gmail.com>
2026-08-30 17:28:27 +0600

    fixed3

--- PacientIK/Program.cs
+++ PacientIK/Program.cs
@@ -62,7 +62,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-app.UseCors("CorsPolicy");
+app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
 
